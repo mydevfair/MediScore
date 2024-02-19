00c4335a4c078209cda4a931ce0d602317455b0c
cfairhurst5680@gmail.com <114191776+mydevfair@users.noreply.github.com>
2024-02-19 15:32:32 +0000

    All criteria met. Bonus functions not added or any error handling

--- MediScoreApp/src/Main.java
+++ MediScoreApp/src/Main.java
@@ -4,17 +4,19 @@ public class Main {
                 Patient.Consciousness.ALERT, 15, 95, 37.15638f);
 
         Patient patient2 = new Patient("Patient 2", Patient.AirOrOxygen.OXYGEN,
-                Patient.Consciousness.ALERT, 17, 95, 37.15638f);
+                Patient.Consciousness.CVPU, 17, 80, 37.15638f);
 
         Patient patient3 = new Patient("Patient 3", Patient.AirOrOxygen.OXYGEN,
-                Patient.Consciousness.CVPU1, 23, 88, 38.54f);
+                Patient.Consciousness.CVPU, 23, 88, 38.54f);
 
-        patient1.mediScoreFunction(patient1);
-        patient2.mediScoreFunction(patient2);
-        patient3.mediScoreFunction(patient3);
+        patient1.calculateMediScore(patient1);
+        patient2.calculateMediScore(patient2);
+        patient3.calculateMediScore(patient3);
 
         System.out.println(patient1);
         System.out.println(patient2);
         System.out.println(patient3);
+
+
     }
 }
